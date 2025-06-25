26ef1e7c50241c6f1e23e3c6ff75feafdad9d930
Gathu17 <jjgathu17@gmail.com>
2025-06-25 18:16:59 +0300

    fix store width

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -3,7 +3,7 @@ import PrintfulProducts from './PrintfulProducts';
 function Store() {
   return (
     <section className="py-20">
-      <div className="container mx-auto px-6">
+      <div className="mx-auto px-6">
         <h2 className="text-white text-4xl font-bold mb-8 text-center section-title animate-slide-up">Our Store</h2>
         <PrintfulProducts />
       </div>
